8a6bf46e880a2d9a866bcd475faacdfe7bad68c4
Hala Karaki <halak@uber.com>
2018-09-14 16:47:15 -0700

    fix spelling

--- src/test/java/com/uber/cadence/internal/replay/ReplayDeciderCacheTests.java
+++ src/test/java/com/uber/cadence/internal/replay/ReplayDeciderCacheTests.java
@@ -78,7 +78,7 @@ public class ReplayDeciderCacheTests {
     // Act
     Decider decider2 = replayDeciderCache.getOrCreate(decisionTask, this::createFakeDecider);
 
-    // Assertp
+    // Assert
     assertEquals(decider2, replayDeciderCache.getUnchecked(runId));
     assertNotSame(decider2, decider);
   }
