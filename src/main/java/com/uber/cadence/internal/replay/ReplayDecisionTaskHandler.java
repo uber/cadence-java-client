3a2dc7899809204f8c26256b954723e8d2d0e4c1
Maxim Fateev <maxim@uber.com>
2018-10-26 15:18:29 -0700

    Fixed metric name

--- src/main/java/com/uber/cadence/internal/replay/ReplayDecisionTaskHandler.java
+++ src/main/java/com/uber/cadence/internal/replay/ReplayDecisionTaskHandler.java
@@ -147,7 +147,7 @@ public final class ReplayDecisionTaskHandler implements DecisionTaskHandler {
                 + " new decisions");
       }
       return createCompletedRequest(decisionTask, decisions);
-    } catch (Exception e) {
+    } catch (Throwable e) {
       if (stickyTaskListName != null) {
         cache.invalidate(decisionTask);
       }
