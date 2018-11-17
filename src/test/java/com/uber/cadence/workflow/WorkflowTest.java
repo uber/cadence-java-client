484011db76ea816c5a4d1052ba554582f97e059a
Maxim Fateev <maxim@uber.com>
2018-11-16 19:26:45 -0800

    Increased test timeout due to Travis

--- src/test/java/com/uber/cadence/workflow/WorkflowTest.java
+++ src/test/java/com/uber/cadence/workflow/WorkflowTest.java
@@ -125,7 +125,7 @@ public class WorkflowTest {
 
   @Rule
   public Timeout globalTimeout =
-      Timeout.seconds(DEBUGGER_TIMEOUTS ? 500 : skipDockerService ? 10 : 30);
+      Timeout.seconds(DEBUGGER_TIMEOUTS ? 500 : skipDockerService ? 20 : 30);
 
   @Rule
   public TestWatcher watchman =
@@ -1226,7 +1226,7 @@ public class WorkflowTest {
     }
   }
 
-  @Test(timeout = 20000)
+  @Test
   public void testChildAsyncWorkflow() {
     startWorkerFor(TestChildAsyncWorkflow.class, TestMultiargsWorkflowsImpl.class);
 
