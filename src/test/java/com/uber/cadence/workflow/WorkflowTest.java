87fa5a23fb7b9b6846def9b5b3abef537c592cb7
Bowei Xu <boweixu@uber.com>
2019-10-15 13:43:28 -0700

    Remove manual test flag

--- src/test/java/com/uber/cadence/workflow/WorkflowTest.java
+++ src/test/java/com/uber/cadence/workflow/WorkflowTest.java
@@ -183,7 +183,7 @@ public class WorkflowTest {
         }
       };
 
-  @Parameter public boolean useExternalService = true;
+  @Parameter public boolean useExternalService;
 
   @Parameter(1)
   public String testType;
