1a1d4d0f74be67b69088c390d1b4596aa3b8ba5d
Maxim Fateev <mfateev@gmail.com>
2020-02-14 18:08:42 -0800

    Fixed timer multithreading logic

--- src/main/java/com/uber/cadence/internal/sync/DeterministicRunnerImpl.java
+++ src/main/java/com/uber/cadence/internal/sync/DeterministicRunnerImpl.java
@@ -249,7 +249,7 @@ class DeterministicRunnerImpl implements DeterministicRunner {
             }
           } else {
             long t = c.getBlockedUntil();
-            if (t > nextWakeUpTime) {
+            if (nextWakeUpTime == 0 || t < nextWakeUpTime) {
               nextWakeUpTime = t;
             }
           }
