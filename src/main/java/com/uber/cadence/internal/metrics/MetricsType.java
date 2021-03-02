b5fd4cdecbe40a915e19222fe6159d49ac5a35b3
Yu Xia <yx@uber.com>
2021-03-01 22:12:13 -0800

    remove metrics type change

--- src/main/java/com/uber/cadence/internal/metrics/MetricsType.java
+++ src/main/java/com/uber/cadence/internal/metrics/MetricsType.java
@@ -161,11 +161,4 @@ public class MetricsType {
 
   public static final String NON_DETERMINISTIC_ERROR =
       CADENCE_METRICS_PREFIX + "non-deterministic-error";
-
-  public static final String SHADOWING_NON_DETERMINISTIC_ERROR =
-          CADENCE_METRICS_PREFIX + "shadowing-non-deterministic-error";
-  public static final String SHADOWING_SKIPPED_COUNT =
-          CADENCE_METRICS_PREFIX + "shadowing-skipped-workflow";
-  public static final String SHADOWING_SUCCESS_COUNT =
-          CADENCE_METRICS_PREFIX + "shadowing-success-workflow";
 }
