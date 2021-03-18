23a51c2df82f6b874778fc5ae4f3a9b44e359a70
Yu Xia <yx@uber.com>
2021-03-17 17:06:59 -0700

    Add header

--- src/main/java/com/uber/cadence/testing/WorkflowShadower.java
+++ src/main/java/com/uber/cadence/testing/WorkflowShadower.java
@@ -1,3 +1,18 @@
+/*
+ *  Modifications Copyright (c) 2017-2021 Uber Technologies Inc.
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
 package com.uber.cadence.testing;
 
 import com.cronutils.utils.VisibleForTesting;
@@ -15,82 +30,89 @@ import com.uber.cadence.shadower.ScanWorkflowActivityResult;
 import com.uber.cadence.worker.ShadowingOptions;
 import com.uber.m3.tally.NoopScope;
 import com.uber.m3.tally.Scope;
-
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 public final class WorkflowShadower {
 
-    private final ShadowingOptions options;
-    private final String query;
-    private final ScanWorkflowActivity scanWorkflow;
-    private final ReplayWorkflowActivity replayWorkflow;
+  private final ShadowingOptions options;
+  private final String query;
+  private final ScanWorkflowActivity scanWorkflow;
+  private final ReplayWorkflowActivity replayWorkflow;
 
-    public WorkflowShadower(IWorkflowService service, ShadowingOptions options, String taskList) {
+  public WorkflowShadower(IWorkflowService service, ShadowingOptions options, String taskList) {
 
-        this(service, options, taskList, new NoopScope());
+    this(service, options, taskList, new NoopScope());
+  }
+
+  public WorkflowShadower(
+      IWorkflowService service, ShadowingOptions options, String taskList, Scope metricsScope) {
+    // TODO: replace the null with taskList
+    this(
+        options,
+        new ScanWorkflowActivityImpl(service),
+        new ReplayWorkflowActivityImpl(service, metricsScope, null));
+  }
+
+  @VisibleForTesting
+  public WorkflowShadower(
+      ShadowingOptions options,
+      ScanWorkflowActivity scanWorkflow,
+      ReplayWorkflowActivity replayWorkflow) {
+    this.options = options;
+    // TODO: generate query from shadowing options
+    this.query = "";
+    this.scanWorkflow = scanWorkflow;
+    this.replayWorkflow = replayWorkflow;
+  }
+
+  public void run() throws Throwable {
+    byte[] nextPageToken = null;
+    int replayCount = 0;
+
+    int maxReplayCount = Integer.MAX_VALUE;
+    Duration maxReplayDuration = ChronoUnit.FOREVER.getDuration();
+    ZonedDateTime now = ZonedDateTime.now();
+    if (options.getExitCondition() != null) {
+      if (options.getExitCondition().getShadowingCount() != 0) {
+        maxReplayCount = options.getExitCondition().getShadowingCount();
+      }
+      if (options.getExitCondition().getShadowingDuration() != null) {
+        maxReplayDuration = options.getExitCondition().getShadowingDuration();
+      }
     }
+    do {
+      ScanWorkflowActivityResult ScanResult =
+          scanWorkflow.scan(
+              new ScanWorkflowActivityParams()
+                  .setDomain(options.getDomain())
+                  .setWorkflowQuery(query)
+                  .setNextPageToken(nextPageToken)
+                  .setSamplingRate(options.getSamplingRate()));
+      nextPageToken = ScanResult.getNextPageToken();
 
-    public WorkflowShadower(IWorkflowService service, ShadowingOptions options, String taskList, Scope metricsScope) {
-        // TODO: replace the null with taskList
-        this(options, new ScanWorkflowActivityImpl(service), new ReplayWorkflowActivityImpl(service, metricsScope, null));
-    }
+      for (WorkflowExecution execution : ScanResult.getExecutions()) {
+        ReplayWorkflowActivityResult replayResult =
+            replayWorkflow.replay(
+                new ReplayWorkflowActivityParams()
+                    .setDomain(options.getDomain())
+                    .setExecutions(Lists.newArrayList(execution)));
 
-    @VisibleForTesting
-    public WorkflowShadower(
-            ShadowingOptions options,
-            ScanWorkflowActivity scanWorkflow,
-            ReplayWorkflowActivity replayWorkflow) {
-        this.options = options;
-        // TODO: generate query from shadowing options
-        this.query = "";
-        this.scanWorkflow = scanWorkflow;
-        this.replayWorkflow = replayWorkflow;
-    }
-
-    public void run() throws Throwable {
-        byte[] nextPageToken = null;
-        int replayCount = 0;
-
-        int maxReplayCount = Integer.MAX_VALUE;
-        Duration maxReplayDuration = ChronoUnit.FOREVER.getDuration();
-        ZonedDateTime now = ZonedDateTime.now();
-        if (options.getExitCondition() != null) {
-            if (options.getExitCondition().getShadowingCount() != 0) {
-                maxReplayCount = options.getExitCondition().getShadowingCount();
-            }
-            if (options.getExitCondition().getShadowingDuration() != null) {
-                maxReplayDuration = options.getExitCondition().getShadowingDuration();
-            }
+        if (replayResult.getFailed() > 0) {
+          throw new Error("Replay workflow history failed with execution:" + execution.toString());
+        } else if (replayResult.getSucceeded() > 0) {
+          replayCount++;
         }
-        do {
-            ScanWorkflowActivityResult ScanResult = scanWorkflow.scan(new ScanWorkflowActivityParams()
-            .setDomain(options.getDomain())
-            .setWorkflowQuery(query)
-            .setNextPageToken(nextPageToken)
-            .setSamplingRate(options.getSamplingRate()));
-            nextPageToken = ScanResult.getNextPageToken();
 
-            for(WorkflowExecution execution : ScanResult.getExecutions()) {
-                ReplayWorkflowActivityResult replayResult = replayWorkflow.replay(new ReplayWorkflowActivityParams()
-                .setDomain(options.getDomain())
-                .setExecutions(Lists.newArrayList(execution)));
-
-                if (replayResult.getFailed() > 0) {
-                    throw new Error("Replay workflow history failed with execution:" + execution.toString());
-                } else if (replayResult.getSucceeded() > 0) {
-                    replayCount++;
-                }
-
-                // Check exit condition
-                if (replayCount >= maxReplayCount) {
-                    return;
-                }
-                if (ZonedDateTime.now().isAfter(now.plus(maxReplayDuration))) {
-                    return;
-                }
-            }
-        } while(nextPageToken != null);
-    }
+        // Check exit condition
+        if (replayCount >= maxReplayCount) {
+          return;
+        }
+        if (ZonedDateTime.now().isAfter(now.plus(maxReplayDuration))) {
+          return;
+        }
+      }
+    } while (nextPageToken != null);
+  }
 }
