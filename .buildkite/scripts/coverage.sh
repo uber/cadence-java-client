d7646d050619a43f22e60bfaa9e149de8462852f
Yu Xia <yx@uber.com>
2021-03-26 08:26:22 -0700

    update step name

--- .buildkite/scripts/coverage.sh
+++ .buildkite/scripts/coverage.sh
@@ -4,7 +4,7 @@ set -ex
 
 # download cover files from all the tests
 mkdir -p build/reports/jacoco/test
-buildkite-agent artifact download "build/reports/jacoco/test/jacocoTestReport.xml" . --step ":Java: unit test" --build "$BUILDKITE_BUILD_ID"
+buildkite-agent artifact download "build/reports/jacoco/test/jacocoTestReport.xml" . --step ":java: Unit test with test services" --build "$BUILDKITE_BUILD_ID"
 
 echo "download complete"
 
