8dc49271f31183d1dbbcc56d9483c327a60c4602
Max K <25259015+mkolodezny@users.noreply.github.com>
2022-07-20 13:24:45 -0700

    add rpc headers to grpc stubs (#679)

--- src/main/java/com/uber/cadence/internal/compatibility/proto/serviceclient/GrpcServiceStubs.java
+++ src/main/java/com/uber/cadence/internal/compatibility/proto/serviceclient/GrpcServiceStubs.java
@@ -50,12 +50,17 @@ final class GrpcServiceStubs implements IGrpcServiceStubs {
   private static final Logger log = LoggerFactory.getLogger(GrpcServiceStubs.class);
   private static final Metadata.Key<String> LIBRARY_VERSION_HEADER_KEY =
       Metadata.Key.of("cadence-client-library-version", Metadata.ASCII_STRING_MARSHALLER);
-
   private static final Metadata.Key<String> FEATURE_VERSION_HEADER_KEY =
       Metadata.Key.of("cadence-client-feature-version", Metadata.ASCII_STRING_MARSHALLER);
-
   private static final Metadata.Key<String> CLIENT_IMPL_HEADER_KEY =
       Metadata.Key.of("cadence-client-name", Metadata.ASCII_STRING_MARSHALLER);
+  private static final Metadata.Key<String> RPC_SERVICE_NAME_HEADER_KEY =
+      Metadata.Key.of("rpc-service", Metadata.ASCII_STRING_MARSHALLER);
+  private static final Metadata.Key<String> RPC_CALLER_NAME_HEADER_KEY =
+      Metadata.Key.of("rpc-caller", Metadata.ASCII_STRING_MARSHALLER);
+  private static final Metadata.Key<String> RPC_ENCODING_HEADER_KEY =
+      Metadata.Key.of("rpc-encoding", Metadata.ASCII_STRING_MARSHALLER);
+
 
   private static final String CLIENT_IMPL_HEADER_VALUE = "uber-java";
 
@@ -83,6 +88,9 @@ final class GrpcServiceStubs implements IGrpcServiceStubs {
     headers.put(LIBRARY_VERSION_HEADER_KEY, Version.LIBRARY_VERSION);
     headers.put(FEATURE_VERSION_HEADER_KEY, Version.FEATURE_VERSION);
     headers.put(CLIENT_IMPL_HEADER_KEY, CLIENT_IMPL_HEADER_VALUE);
+    headers.put(RPC_SERVICE_NAME_HEADER_KEY, options.getServiceName());
+    headers.put(RPC_CALLER_NAME_HEADER_KEY, CLIENT_IMPL_HEADER_VALUE);
+    headers.put(RPC_ENCODING_HEADER_KEY, "proto");
     Channel interceptedChannel =
         ClientInterceptors.intercept(
             channel, deadlineInterceptor, MetadataUtils.newAttachHeadersInterceptor(headers));
