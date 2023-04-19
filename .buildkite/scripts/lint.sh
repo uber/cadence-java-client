281cdfa607164a92282588c87dce99c712e64006
Abhishek Jha <abhishek.jha@uber.com>
2023-04-19 12:17:41 -0700

    wip

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -5,7 +5,7 @@ set -ex
 ./gradlew goJF
 
 if [ -n "$(git status --porcelain)" ]; then
-  echo "There are changes after make go-generate && make fmt && make lint && make copyright"
+  echo "There are changes after linting (used goJF) cmd: ./gradlew goJF"
   echo "Please rerun the command and commit the changes"
   git status --porcelain
   exit 1
