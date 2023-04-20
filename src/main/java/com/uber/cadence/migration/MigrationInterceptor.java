472b141b4323a59a3c94a67000e942a4d899b563
Abhishek Jha <abhishek.jha@uber.com>
2023-04-20 13:13:42 -0700

    added MigrationInterceptor class and versioning

--- src/main/java/com/uber/cadence/migration/MigrationInterceptor.java
+++ src/main/java/com/uber/cadence/migration/MigrationInterceptor.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
 package com.uber.cadence.migration;
 
 import com.uber.cadence.internal.sync.SyncWorkflowDefinition;
@@ -7,21 +24,24 @@ import com.uber.cadence.workflow.WorkflowInterceptorBase;
 
 public class MigrationInterceptor extends WorkflowInterceptorBase {
 
-    private final WorkflowInterceptor next;
-    private static final String versionChangeID = "cadenceMigrationInterceptor";
-    public static final int versionV1 = 1;
-    public MigrationInterceptor(WorkflowInterceptor next) {
-        super(next);
-        this.next = next;
+  private final WorkflowInterceptor next;
+  private static final String versionChangeID = "cadenceMigrationInterceptor";
+  public static final int versionV1 = 1;
+
+  public MigrationInterceptor(WorkflowInterceptor next) {
+    super(next);
+    this.next = next;
+  }
+
+  @Override
+  public byte[] executeWorkflow(
+      SyncWorkflowDefinition workflowDefinition, WorkflowExecuteInput input) {
+
+    int version = getVersion(versionChangeID, Workflow.DEFAULT_VERSION, versionV1);
+    switch (version) {
+      case versionV1:
+      default:
+        return next.executeWorkflow(workflowDefinition, input);
     }
-
-    @Override
-    public byte[] executeWorkflow(SyncWorkflowDefinition workflowDefinition, WorkflowExecuteInput input) {
-
-        int version = getVersion(versionChangeID, Workflow.DEFAULT_VERSION, versionV1);
-
-
-        return next.executeWorkflow(workflowDefinition,input);
-    }
-
+  }
 }
