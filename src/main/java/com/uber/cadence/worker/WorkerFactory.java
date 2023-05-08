c495e7b8bb531d4552ade395bc01e8fc758a856e
duoertai <duoertai@gmail.com>
2023-05-08 15:28:19 -0700

    fix error message for worker factory (#788)

--- src/main/java/com/uber/cadence/worker/WorkerFactory.java
+++ src/main/java/com/uber/cadence/worker/WorkerFactory.java
@@ -199,7 +199,7 @@ public final class WorkerFactory {
             statusErrorMessage,
             "start WorkerFactory",
             state.name(),
-            String.format("%s, %s", State.Initial.name(), State.Initial.name())));
+            String.format("%s, %s", State.Initial.name(), State.Started.name())));
     if (state == State.Started) {
       return;
     }
