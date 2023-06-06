4d4acf4ed37bb3553d8b6ba28a0675c6b6d49b7e
Abhishek Jha <abhishek.jha@uber.com>
2023-06-06 15:05:56 -0700

    Implementation: QueryWorkflow and CountWorkflowExecutions

--- src/main/java/com/uber/cadence/migration/MigrationIWorkflowService.java
+++ src/main/java/com/uber/cadence/migration/MigrationIWorkflowService.java
@@ -124,10 +124,9 @@ public class MigrationIWorkflowService extends DummyIWorkflowService {
     ListWorkflowExecutionsResponse response = new ListWorkflowExecutionsResponse();
     if (searchType == _listWorkflow) {
       response = serviceNew.ListWorkflowExecutions(listWorkflowExecutionsRequest);
+    } else if (searchType == _scanWorkflow) {
+      response = serviceNew.ListWorkflowExecutions(listWorkflowExecutionsRequest);
     }
-    else if(searchType == _scanWorkflow) {
-          response = serviceNew.ListWorkflowExecutions(listWorkflowExecutionsRequest);
-        }
     return response;
   }
 
@@ -230,5 +229,24 @@ public class MigrationIWorkflowService extends DummyIWorkflowService {
       return response;
     }
     return callOldCluster(listRequest, 0, _scanWorkflow);
+  }
+
+  @Override
+  public QueryWorkflowResponse QueryWorkflow(QueryWorkflowRequest queryRequest) throws TException {
+    if (shouldStartInNew(queryRequest.getExecution().getWorkflowId()))
+      return serviceNew.QueryWorkflow(queryRequest);
+    return serviceOld.QueryWorkflow(queryRequest);
+  }
+
+  @Override
+  public CountWorkflowExecutionsResponse CountWorkflowExecutions(
+      CountWorkflowExecutionsRequest countRequest) throws TException {
+
+    CountWorkflowExecutionsResponse countResponseNew =
+        serviceNew.CountWorkflowExecutions(countRequest);
+    CountWorkflowExecutionsResponse countResponseOld =
+        serviceOld.CountWorkflowExecutions(countRequest);
+    countResponseOld.setCount(countResponseOld.getCount() + countResponseNew.getCount());
+    return countResponseOld;
   }
 }
