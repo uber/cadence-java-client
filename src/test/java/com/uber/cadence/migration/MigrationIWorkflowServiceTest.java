a3cf125b42df07e6091ce28ea8a4f0a47df33193
Abhishek Jha <abhishek.jha@uber.com>
2023-07-27 13:05:49 -0700

    Adding unit test: response null in migrationService

--- src/test/java/com/uber/cadence/migration/MigrationIWorkflowServiceTest.java
+++ src/test/java/com/uber/cadence/migration/MigrationIWorkflowServiceTest.java
@@ -3,9 +3,7 @@ package com.uber.cadence.migration;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-import com.uber.cadence.StartWorkflowExecutionRequest;
-import com.uber.cadence.StartWorkflowExecutionResponse;
-import com.uber.cadence.WorkflowType;
+import com.uber.cadence.*;
 import com.uber.cadence.serviceclient.IWorkflowService;
 import org.apache.thrift.TException;
 import org.junit.Before;
@@ -21,7 +19,8 @@ public class MigrationIWorkflowServiceTest {
 
   @Mock private IWorkflowService serviceNew;
 
-  @Captor private ArgumentCaptor<StartWorkflowExecutionRequest> requestCaptor;
+  @Captor private ArgumentCaptor<StartWorkflowExecutionRequest> startRequestCaptor;
+  @Captor private ArgumentCaptor<ListWorkflowExecutionsRequest> listRequestCaptor;
 
   private MigrationIWorkflowService migrationService;
 
@@ -33,48 +32,78 @@ public class MigrationIWorkflowServiceTest {
   }
 
   @Test
-  public void testStartWorkflowExecution_WhenShouldStartInNew_ReturnsResponseFromServiceNew()
-      throws TException {
-    // Arrange
+  public void testStartWorkflowExecution_startNewWorkflow() throws TException {
+
     StartWorkflowExecutionRequest startRequest =
         new StartWorkflowExecutionRequest()
             .setWorkflowId("123")
             .setWorkflowType(new WorkflowType().setName("sampleWorkflow"))
             .setRequestId("123");
 
-    when(migrationService.shouldStartInNew(startRequest.getWorkflowId())).thenReturn(true);
-
     StartWorkflowExecutionResponse responseNew = new StartWorkflowExecutionResponse();
     when(serviceNew.StartWorkflowExecution(startRequest)).thenReturn(responseNew);
 
-    // Act
     StartWorkflowExecutionResponse response = migrationService.StartWorkflowExecution(startRequest);
 
-    // Assert
     assertEquals(responseNew, response);
-    verify(serviceNew, times(1)).StartWorkflowExecution(requestCaptor.capture());
-    assertEquals(startRequest, requestCaptor.getValue());
+    verify(serviceNew, times(1)).StartWorkflowExecution(startRequestCaptor.capture());
+    assertEquals(startRequest, startRequestCaptor.getValue());
   }
 
-  //  @Test
-  //  public void testStartWorkflowExecution_WhenShouldStartInOld_ReturnsResponseFromServiceOld()
-  //      throws TException {
-  //    // Arrange
-  //    StartWorkflowExecutionRequest startRequest =
-  //        new StartWorkflowExecutionRequest().setWorkflowId("123").setWorkflowType("wfType");
-  //
-  //    when(migrationService.shouldStartInNew(startRequest.getWorkflowId())).thenReturn(false);
-  //
-  //    StartWorkflowExecutionResponse responseOld = new StartWorkflowExecutionResponse();
-  //    when(serviceOld.StartWorkflowExecution(startRequest)).thenReturn(responseOld);
-  //
-  //    // Act
-  //    StartWorkflowExecutionResponse response =
-  // migrationService.StartWorkflowExecution(startRequest);
-  //
-  //    // Assert
-  //    assertEquals(responseOld, response);
-  //    verify(serviceOld, times(1)).StartWorkflowExecution(requestCaptor.capture());
-  //    assertEquals(startRequest, requestCaptor.getValue());
-  //  }
+  @Test
+  public void testStartWorkflowExecution_startOldWorkflow() throws TException {
+
+    StartWorkflowExecutionRequest startRequest =
+        new StartWorkflowExecutionRequest()
+            .setWorkflowId("1234")
+            .setWorkflowType(new WorkflowType().setName("sampleWorkflow"))
+            .setRequestId("123");
+
+    // when(migrationService.shouldStartInNew(startRequest.getWorkflowId())).thenReturn(false);
+
+    StartWorkflowExecutionResponse responseOld = new StartWorkflowExecutionResponse();
+    when(serviceOld.StartWorkflowExecution(startRequest)).thenReturn(responseOld);
+
+    // Act
+    StartWorkflowExecutionResponse response = migrationService.StartWorkflowExecution(startRequest);
+    // response = serviceOld.StartWorkflowExecution(startRequest);
+    // Assert
+    assertEquals(responseOld, response);
+    verify(serviceOld, times(1)).StartWorkflowExecution(startRequestCaptor.capture());
+    assertEquals(startRequest, startRequestCaptor.getValue());
+  }
+
+  @Test
+  public void testListWorkflowExecutions_startNewWorkflow() throws TException {
+
+    ListWorkflowExecutionsRequest listRequest =
+        new ListWorkflowExecutionsRequest().setDomain("domain").setPageSize(10);
+    listRequest.setNextPageToken("sample workflow".getBytes());
+
+    ListWorkflowExecutionsResponse responseNew = new ListWorkflowExecutionsResponse();
+    when(serviceNew.ListWorkflowExecutions(listRequest)).thenReturn(responseNew);
+
+    ListWorkflowExecutionsResponse response = migrationService.ListWorkflowExecutions(listRequest);
+
+    // assertNotNull("Response is null", response);
+    assertEquals(responseNew, response);
+    verify(serviceNew, times(1)).ListWorkflowExecutions(listRequestCaptor.capture());
+    //    assertEquals(listRequest, listRequestCaptor.getValue());
+  }
+
+  @Test
+  public void testListWorkflowExecutions_startOldWorkflow() throws TException {
+
+    ListWorkflowExecutionsRequest listRequest =
+        new ListWorkflowExecutionsRequest().setDomain("domain").setPageSize(10);
+
+    ListWorkflowExecutionsResponse responseOld = new ListWorkflowExecutionsResponse();
+    when(serviceOld.ListWorkflowExecutions(listRequest)).thenReturn(responseOld);
+
+    ListWorkflowExecutionsResponse response = migrationService.ListWorkflowExecutions(listRequest);
+
+    assertEquals(responseOld, response);
+    verify(serviceOld, times(1)).ListWorkflowExecutions(listRequestCaptor.capture());
+    assertEquals(listRequest, listRequestCaptor.getValue());
+  }
 }
