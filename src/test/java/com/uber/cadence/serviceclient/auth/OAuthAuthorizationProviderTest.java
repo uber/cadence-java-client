9a2799e4c60bc436ef2ba556a0ed430a46fda0f2
Mantas Sidlauskas <mantass@netapp.com>
2024-03-21 15:11:02 +0200

    lint

--- src/test/java/com/uber/cadence/serviceclient/auth/OAuthAuthorizationProviderTest.java
+++ src/test/java/com/uber/cadence/serviceclient/auth/OAuthAuthorizationProviderTest.java
@@ -1,10 +1,11 @@
 package com.uber.cadence.serviceclient.auth;
 
 import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class OAuthAuthorizationProviderTest   {
+public class OAuthAuthorizationProviderTest {
   @Test
   public void testConstructorWillThrowException() {
     try {
