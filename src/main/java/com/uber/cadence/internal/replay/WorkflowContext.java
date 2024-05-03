801d9b4bf9974be675c522dcc7c985acb3729082
Abhishek Jha <abhishekj720@users.noreply.github.com>
2024-05-03 11:34:46 -0700

    Bug Fix for null pointer exception in case header getFields is null in workflow context replay (#891)
    * Fix for null pointer exception

--- src/main/java/com/uber/cadence/internal/replay/WorkflowContext.java
+++ src/main/java/com/uber/cadence/internal/replay/WorkflowContext.java
@@ -156,13 +156,16 @@ final class WorkflowContext {
       return new HashMap<>();
     }
 
+    Map<String, ByteBuffer> fields = headers.getFields();
+    if (fields == null) {
+      return new HashMap<>();
+    }
+
     Map<String, byte[]> headerData = new HashMap<>();
-    headers
-        .getFields()
-        .forEach(
-            (k, v) -> {
-              headerData.put(k, org.apache.thrift.TBaseHelper.byteBufferToByteArray(v));
-            });
+    fields.forEach(
+        (k, v) -> {
+          headerData.put(k, org.apache.thrift.TBaseHelper.byteBufferToByteArray(v));
+        });
 
     Map<String, Object> contextData = new HashMap<>();
     for (ContextPropagator propagator : contextPropagators) {
