ad20d9736e1e21378eeb1990b751b185c1c9b561
Shijie Sheng <liouvetren@gmail.com>
2024-06-12 11:10:30 -0700

    Fix incorrect span activation for local activities (#910)
    What changed?
    
    span is incorrectly activated for local activities, this is already activated in activity worker
    
    [cadence-java-client/src/main/java/com/uber/cadence/internal/worker/LocalActivityWorker.java](https://github.com/uber/cadence-java-client/blob/95cb139ff9947db730186369a4f4ed34578f85c8/src/main/java/com/uber/cadence/internal/worker/LocalActivityWorker.java#L137)

--- src/main/java/com/uber/cadence/internal/tracing/TracingPropagator.java
+++ src/main/java/com/uber/cadence/internal/tracing/TracingPropagator.java
@@ -101,17 +101,14 @@ public class TracingPropagator {
     // retrieve spancontext from params
     SpanContext parent = extract(params.getContext());
 
-    Span span =
-        tracer
-            .buildSpan(EXECUTE_LOCAL_ACTIVITY)
-            .ignoreActiveSpan()
-            .addReference(References.FOLLOWS_FROM, parent)
-            .withTag(TAG_WORKFLOW_ID, params.getWorkflowExecution().getWorkflowId())
-            .withTag(TAG_WORKFLOW_RUN_ID, params.getWorkflowExecution().getRunId())
-            .withTag(TAG_ACTIVITY_TYPE, params.getActivityType().getName())
-            .start();
-    tracer.activateSpan(span);
-    return span;
+    return tracer
+        .buildSpan(EXECUTE_LOCAL_ACTIVITY)
+        .ignoreActiveSpan()
+        .addReference(References.FOLLOWS_FROM, parent)
+        .withTag(TAG_WORKFLOW_ID, params.getWorkflowExecution().getWorkflowId())
+        .withTag(TAG_WORKFLOW_RUN_ID, params.getWorkflowExecution().getRunId())
+        .withTag(TAG_ACTIVITY_TYPE, params.getActivityType().getName())
+        .start();
   }
 
   public void inject(Map<String, byte[]> headers) {
